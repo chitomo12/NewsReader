b15d24b12ddd75e071f51bee389ec6b745dc80f1
chitomo12 <mrk3_12@yahoo.co.jp>
2021-11-03 19:27:03 +0900

    commit test

---
+++ NewsReader/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by 福田正知 on 2021/11/03.
+//
+
+//import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Storyboard上で「Cell」とアイデンティファイしたアイテムと関連づける
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
