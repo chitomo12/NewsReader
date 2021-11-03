7c2de9706dc5c727ae7e43c5aba26f4aa7cfcca7
chitomo12 <mrk3_12@yahoo.co.jp>
2021-11-03 20:58:44 +0900

    Detail

---
+++ NewsReader/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  NewsReader
+//
+//  Created by 福田正知 on 2021/11/03.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+}
